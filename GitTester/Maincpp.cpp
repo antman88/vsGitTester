eed76a0283625d9855ff643c0f053a1208786b57
anton <mail@>
2019-07-20 20:23:31 +0300

    commit3: add 3 developer and make print routine

--- GitTester/Maincpp.cpp
+++ GitTester/Maincpp.cpp
@@ -2,13 +2,21 @@
 
 using namespace std;
 
+void  print();
+
 int main()
 {
 	//
-	cout << "Hello. I am a developer 1" << endl;
-	cout << "Hello, I am a developer 2" << endl;
+	print();
 
 	//
 	system("pause");
 	return 0;
+}
+
+void  print()
+{
+	cout << "Hello. I am a developer 1" << endl;
+	cout << "Hello, I am a developer 2" << endl;
+	cout << "Hello, I am a developer 3" << endl;
 }
